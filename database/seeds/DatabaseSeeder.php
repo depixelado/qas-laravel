a71fb8f268dad39d046bdcdc37dde54b8d820bf1
Daniel Jimenez <jimenezdaniel87@gmail.com>
2017-01-23 18:14:53 +0000

    Refactor DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,14 +5,23 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Tables which will be trucated
-     * @var [type]
+     * Tables which will be truncated
+     * @var array
      */
     protected $toTruncate = [
         'users',
         'questions',
         'answers',
         'ratings',
+    ];
+
+    /**
+     * [$toSeed description]
+     * @var array
+     */
+    protected $toSeed = [
+        UsersTableSeeder::class,
+        QuestionsTableSeeder::class
     ];
 
     /**
@@ -27,16 +36,30 @@ class DatabaseSeeder extends Seeder
         // Disable foreign key check for this connection before running seeders
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        // Truncate specified DBs
-        foreach ($this->toTruncate as $table) {
-            DB::table($table)->truncate();
-        }
-
-        $this->call(UsersTableSeeder::class);
-        $this->call(QuestionsTableSeeder::class);
+        $this->truncateTables();
+        $this->seedTables();
 
         // Supposed to only apply to a single connection and reset itself
         // but explicitly for clarity
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    /**
+     * Truncate specified tables
+     */
+    private function truncateTables(){
+        // Truncate specified tables
+        foreach ($this->toTruncate as $table) {
+            DB::table($table)->truncate();
+        }
+    }
+
+    /**
+     * Seed Tables
+     */
+    private function seedTables(){
+        foreach ($this->toSeed as $seed) {
+            $this->call($seed);
+        }
     }
 }
